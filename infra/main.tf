f5a7f73242156ed84eef5edcf83a96b0eae803f9
jamcatbiz <james.cate.biz@gmail.com>
2024-09-28 16:19:56 -0400

    fix(infra): updating s3 origin to website endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -97,7 +97,7 @@ resource "aws_cloudfront_distribution" "this" {
 
   # Weordl Origin
   origin {
-    domain_name              = aws_s3_bucket_website_configuration.this.website_domain
+    domain_name              = aws_s3_bucket_website_configuration.this.website_endpoint
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
     origin_id                = local.weordl_origin_id
   }
